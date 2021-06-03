4d2b7db4580d53c9a4576c9792b65f039052370f
don-garu <runik9637@gmail.com>
2021-06-03 11:49:36 +0900

    Account Access FE
    - 로그인 시 내부 페이지로 이동
        계정 ID, PWD 인증은 추후 구현 예정
    - 계정 생성 시 Index 페이지로 다시 이동
        계정 생성 정보 저장은 추후 구현 예정

--- scripts/access.js
+++ scripts/access.js
@@ -1,8 +1,12 @@
 const loginBtn = document.querySelector('.js-loginBtn');
 const signUpBtn = document.querySelector('.js-signUpBtn');
+const cancelBtns = document.querySelectorAll('.js-cancelBtn');
+
 const loginModal = document.querySelector('.js-loginModal');
 const signUpModal = document.querySelector('.js-signUpModal');
-const cancelBtns = document.querySelectorAll('.js-cancelBtn');
+
+const loginForm = document.querySelector('.js-loginForm');
+const signUpForm = document.querySelector('.js-signUpForm');
 
 function modalOpen(modal) {
   modal.style.display = 'block';
@@ -25,12 +29,58 @@ function handleBtnClick(event) {
   }
 }
 
-function init() {
+function addButtonHandler() {
   loginBtn.addEventListener('click', handleBtnClick);
   signUpBtn.addEventListener('click', handleBtnClick);
   cancelBtns.forEach(btn => {
     btn.addEventListener('click', handleBtnClick);
-  })
+  });
+}
+
+function redirectToInnerPage() {
+  document.location.href = 'inner.html';
+}
+
+function redirectToIndexPage() {
+  document.location.href = 'index.html';
+}
+
+function handleLoginSubmit(event) {
+  event.preventDefault();
+  let inputs = event.target.querySelectorAll('input');
+  let uname = inputs[0].value;
+  let upwd = inputs[1].value;
+
+  // ToDo. Authorization
+  // Asis. Just Save User Infos in localStorage / Tobe. Save Session Info in LocalStorage
+  localStorage.setItem('userName', uname);
+  localStorage.setItem('userPwd', upwd);
+
+  redirectToInnerPage();
+}
+
+function handleSignUp(event) {
+  event.preventDefault();
+  let inputs = event.target.querySelectorAll('input');
+  let name = inputs[0].value;
+  let uname = inputs[1].value;
+  let upwd = inputs[2].value;
+
+  // ToDo. Sign Up
+  // Asis. Redirect to Index Page / Tobe. Save Account Information in DB or Something.
+
+  redirectToIndexPage();
+}
+
+function addFormAction() {
+  console.dir(loginForm);
+  loginForm.addEventListener('submit', handleLoginSubmit);
+  signUpForm.addEventListener('submit', handleSignUp);
+}
+
+function init() {
+  addButtonHandler();
+  addFormAction();
 }
 
 init();
